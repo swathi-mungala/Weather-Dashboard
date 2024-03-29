a3dd69cbb674d4ee47542a274ac0f951e2b2b57b
swathi-mungala <mswathi584@gmail.com>
2024-03-29 22:25:05 +0000

    Added basic js code and css for card styling

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    var searchButton = $("#search-button");
+    var cityInput = $("#search-input");
+    var citiesButtons = $("#history");
+    var APIKey = "d5e929ebd485e2474ada0a4ded0b22e5";
+    var weatherToday = $("#today");
+    var weatherForecast = $("#forecast");
+    // Using date from Dayjs library
+    var currentDate = dayjs().format("DD/MM/YYYY"); 
+
+  });
